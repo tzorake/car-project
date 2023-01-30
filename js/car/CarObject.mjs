506ca4e52ac5be0d767b2346ae1a5cf3a36a829b
tzorake <te.a4@yandex.ru>
2023-01-30 21:16:58 +0400

    Rework car model

--- js/car/CarObject.mjs
+++ js/car/CarObject.mjs
@@ -39,113 +39,32 @@ export class CarObject extends GameObject
         this.controller = new CarObjectController(this);
         this.controller.connect();
      
-        this.#debugWidget = new DebugInfo(this, ['position', 'scale', 'velocity', 'acceleration', 'angle'], new Rectangle(10, 10, 275, 200));
+        this.#debugWidget = new DebugInfo(this, ['position', 'scale', 'velocity', 'heading', 'angle'], new Rectangle(10, 10, 275, 200));
 
-        this.angle = 0.0
-        this.length = this.scale.x;
-        this.width = this.scale.y;
-        this.brake_deceleration = 30000
-        this.free_deceleration = 2
-
-        this.gear = CarGear.ZERO;
-        this.throttle = 0
-        this.brakes = 0
-        this.wheel_rpm = 0
-        this.angular_velocity = 0
-        this.force = new Vector2D(0, 0)
-
-        this.rpm_lut = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
-        this.torque_lut = [200, 325, 475, 550, 550, 500, 375, 300, 0];
-        this.rpm = 1000;
-        this.diff_ratio = 3.42
-        this.n = 0.8
-        this.wheel_radius = 0.35
-        this.mass = 1100
-        this.c_drag = 0.4257
-        this.cornering_stiffness_f = -5.0
-        this.cornering_stiffness_r = -5.2
-        this.max_grip = 2.0
+        this.maxSpeed = 25;
+        this.maxAngle = 2*Math.PI
+        this.drag = 0.975
+        this.traction = 0.95;
     }
 
-    update(dt) 
+    update(dt)
     {
         const controller = this.controller;
+        
+        controller.update();
+        
         const direction = controller.direction;
         const actions = controller.actions;
 
-        controller.update();
+        this.velocity = this.velocity.add(this.heading.multiplyScalar(direction.horizontal*dt));
+        this.position = this.position.add(this.velocity);
 
-        if (this.rpm < 2000)
-        {
-            this.rpm = 2000;
-        }
-        const torque = MathFunction.interp(this.rpm, this.rpm_lut, this.torque_lut);
-        const throttle = (actions.keyPressed[CarAction.THROTTLE] | actions.keyPressed[CarAction.REVERSE]) * Math.abs(direction.horizontal);
-        const steer = (actions.keyPressed[CarAction.TURN_RIGHT] | actions.keyPressed[CarAction.TURN_LEFT]) * direction.vertical;
-        
-        this.gear = actions.keyPressed[CarAction.REVERSE] ? CarGear.REVERSE : (actions.keyPressed[CarAction.THROTTLE] ? CarGear.FIRST : CarGear.ZERO);
+        this.velocity = this.velocity.multiplyScalar(this.drag);
+        this.velocity = this.velocity.clampMagnitude(this.maxSpeed);
 
-        const steering = MAX_STEERING_ANGLE * steer;
+        this.angle   += this.velocity.magnitude() * this.maxAngle * direction.vertical * dt;
 
-        const traction_force = torque * this.diff_ratio * throttle * CarGears[this.gear] * (this.n / this.wheel_radius) - 
-                               this.brake_deceleration * this.brakes * Math.sign(this.velocity.x);
-
-        const resistance_force = new Vector2D(-this.c_drag * this.velocity.x * Math.abs(this.velocity.x) - 12.8 * this.velocity.x,
-                                              -this.c_drag * this.velocity.y * Math.abs(this.velocity.y) - 12.8 * this.velocity.y)
-
-        if (this.velocity.x > 5.55)
-        {
-            const yawspeed = this.angular_velocity * 2;
-
-            const [rot_angle, sideslip] = this.velocity.x == 0 ? [0, 0] : [Math.atan2(yawspeed, this.velocity.x), Math.atan2(this.velocity.y, this.velocity.x)];
-
-            const slipanglefront = sideslip + rot_angle - steering;
-            const slipanglerear = sideslip - rot_angle;
-
-            const flatf = new Vector2D(0, 0);
-            const flatr = new Vector2D(0, 0);
-
-            flatf.x = 0;
-            flatf.y = this.cornering_stiffness_f * slipanglefront;
-            flatf.y = Math.min(this.max_grip, flatf.y);
-            flatf.y = Math.max(-this.max_grip, flatf.y);
-            flatf.y *= this.mass * 4.9;
-
-            flatr.x = 0;
-            flatr.y = this.cornering_stiffness_r * slipanglerear;
-            flatr.y = Math.min(this.max_grip, flatr.y);
-            flatr.y = Math.max(-this.max_grip, flatr.y);
-            flatr.y *= this.mass * 4.9;
-
-            this.force.x = traction_force + Math.sin(steering) * flatf.x + flatr.x + resistance_force.x
-            this.force.y = Math.cos(steering) * flatf.y + flatr.y + resistance_force.y
-
-            const torque = 1.5 * (flatf.y - flatr.y);
-
-            this.acceleration = this.force.multiplyScalar(1.0 / this.mass);
-            this.wheel_rpm = this.velocity.x / this.wheel_radius * 30 / Math.PI;
-
-            this.velocity = this.velocity.add(this.acceleration.multiplyScalar(dt));
-            this.position = this.position.add(this.velocity.rotated(this.angle).multiplyScalar(dt));
-
-            const angular_acceleration = torque / 1000;
-            this.angular_velocity += angular_acceleration * dt;
-            this.angle += this.angular_velocity * dt;
-        }
-        else
-        {
-            this.force.x = traction_force + resistance_force.x;
-            this.force.y = resistance_force.y;
-            this.acceleration = this.force.multiplyScalar(1.0 / this.mass);
-            this.wheel_rpm = this.velocity.x / this.wheel_radius * 30 / Math.PI;
-
-            this.velocity = this.velocity.add(this.acceleration.multiplyScalar(dt));
-
-            const angular_velocity = steering ? this.velocity.x / (this.length / Math.tan(steering)) : 0;
-
-            this.position = this.position.add(this.velocity.rotated(this.angle).multiplyScalar(dt));
-            this.angle += angular_velocity * dt;
-        }
+        this.velocity = Vector2D.lerp(this.velocity.normalized(), this.heading.multiplyScalar(this.velocity.magnitude()), this.traction*dt)//.multiplyScalar(this.velocity.magnitude())
     }
 
     get position()
--- js/car/CarObjectRenderer.mjs
+++ js/car/CarObjectRenderer.mjs
@@ -14,58 +14,21 @@ export class CarObjectRenderer extends GameObjectRenderer
     render()
     {
         super.render();
+
+        const gameObject = this.gameObject;
+
+        const context = GameUtils.CONTEXT;
+        const scaleFactor = GameUtils.SCALE;
+        const position = gameObject.position.multiplyScalar(scaleFactor);
+        const velocity = gameObject.velocity.multiplyScalar(scaleFactor*scaleFactor);
+
+        const end = position.add(velocity);
+
+        context.strokeStyle = 'rgba(0, 255, 255, 1.0)';
+        context.beginPath();
+        context.moveTo(position.x, position.y);
+        context.lineTo(end.x, end.y);
+        context.stroke();
+
     }
-
-    // render()
-    // {
-    //     const gameObject = this.gameObject;
-
-    //     if (!TypeChecker.isGameObject(gameObject))
-    //     {
-    //         throw new Error('GameObjectRenderer.render(gameObject) : Passed object must be inherited from `GameObject`!');
-    //     }
-
-    //     const canvas = GameUtils.CANVAS;
-    //     const context = GameUtils.CONTEXT;
-    //     const scaleFactor = 10;
-    //     const position = gameObject.position.multiplyScalar(scaleFactor);
-    //     const heading = gameObject.heading;
-    //     const offset = new Vector2D(canvas.width / 2, canvas.height /2);
-
-    //     GameUtils.BACKGROUND('rgba(0, 0, 0, 1.0)');
-
-    //     const vertices = gameObject.vertices().map(item => item.multiplyScalar(scaleFactor).add(offset));
-    //     const object = new Path2D(`M ${vertices[0].x} ${vertices[0].y} 
-    //                                L ${vertices[1].x} ${vertices[1].y} 
-    //                                L ${vertices[2].x} ${vertices[2].y} 
-    //                                L ${vertices[3].x} ${vertices[3].y} Z`);
-    //     context.fillStyle = 'rgba(127, 0, 255, 1.0)';
-    //     context.strokeStyle = 'rgba(0, 0, 0, 1.0)';
-    //     context.fill(object);
-
-    //     const xAxis = heading.multiplyScalar(scaleFactor);
-    //     const xEnd = position.add(xAxis);
-
-    //     const yAxis = heading.rotated(Math.PI/2).multiplyScalar(scaleFactor);
-    //     const yEnd = position.add(yAxis);
-
-    //     context.lineWidth = 0.5;
-    //     context.strokeStyle = 'rgba(255, 0, 0, 1.0)';
-    //     context.beginPath();
-    //     context.moveTo(position.x, position.y);
-    //     context.lineTo(xEnd.x, xEnd.y);
-    //     context.stroke();
-
-    //     context.strokeStyle = 'rgba(0, 0, 255, 1.0)';
-    //     context.beginPath();
-    //     context.moveTo(position.x, position.y);
-    //     context.lineTo(yEnd.x, yEnd.y);
-    //     context.stroke();
-
-    //     context.beginPath();
-    //     context.arc(position.x, position.y, 0.5, 0, 2*Math.PI);
-    //     context.fillStyle = 'rgba(0, 255, 0, 0.6)'; 
-    //     context.strokeStyle = 'rgba(0, 0, 0, 1.0)';
-    //     context.fill();
-    // }
 }
